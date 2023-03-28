fbbf137818b415f4032c29c58e171cfb450f05d4
Cesar Agudelo <Cesaragudelo@gmail.com>
2023-03-27 20:33:56 -0500

    se crea proyecto 27-03-2023

---
+++ API.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import {rutas} from './routes/rutas.js'
+
+export class API {
+
+    constructor(){
+        this.app = express()
+        this.enrutarPeticiones()
+    }
+    levantarServidor(){
+        this.app.listen(this.levantarServidor.PORT,()=>console.log(`encendido en ${process.env.PORT}`)
+        )
+    }
+    enrutarPeticiones(){
+        this.app.use('/',rutas)
+       
+    }
+    conectarConBD(){}
+
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import {API} from './API.js'
+import  *  as  dotenv  from  'dotenv'   
+dotenv . config ( ) 
+
+
+
+let servidorhotel=new API()
+servidorhotel.levantarServidor()
